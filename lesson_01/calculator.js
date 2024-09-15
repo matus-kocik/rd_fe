0fa8d5e1f0d183ffbd35f8dbf2375bad33ae15f6
Matus <matuskocik@gmail.com>
2024-09-15 06:10:38 +0200

    add calculator.js

---
+++ lesson_01/calculator.js
@@ -0,0 +1,12 @@
+let numberOne = -1;
+let numberTwo = 0;
+
+const add = numberOne + numberTwo;
+const substract = numberOne - numberTwo;
+const multiply = numberOne * numberTwo;
+const divide = numberOne / numberTwo;
+
+console.log(add);
+console.log(substract);
+console.log(multiply);
+console.log(divide);
