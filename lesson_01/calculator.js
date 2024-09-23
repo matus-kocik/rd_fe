b44b1f3377458ccd58ac5adeabdda293cbdd33f0
Matus <matuskocik@gmail.com>
2024-09-23 06:52:15 +0200

    added statement with division by zero

--- lesson_01/calculator.js
+++ lesson_01/calculator.js
@@ -1,10 +1,15 @@
-let numberOne = 0.3;
-let numberTwo = 0.2;
+const numberOne = 0.3;
+const numberTwo = 0;
 
 const add = numberOne + numberTwo;
 const substract = numberOne - numberTwo;
 const multiply = numberOne * numberTwo;
-const divide = numberOne / numberTwo;
+let divide;
+if (numberTwo !== 0) {
+    divide = numberOne / numberTwo;
+} else {
+    divide = "Error - Division by zero";
+}
 
 console.log(add);
 console.log(substract);
