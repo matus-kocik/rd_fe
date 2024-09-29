156d001ca6039dfc43775ba2cd3c300ae8978d03
Matus <matuskocik@gmail.com>
2024-09-29 20:21:39 +0200

    created first version calculator, but no finish

---
+++ lesson_01/script.js
@@ -0,0 +1,31 @@
+const inputValue = document.getElementById("userinput");
+
+const calculate = document.querySelectorAll(".operations").forEach(function (item) {
+    item.addEventListener("click", function (e) {
+        let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
+
+        if (!isNaN(lastValue) && e.target.innerText === "=") {
+            inputValue.innerText = eval(inputValue.innerText);
+        } else if (e.target.innerText === "AC") {
+            inputValue.innerText = "0";
+        } else if (e.target.innerText === "DEL") {
+            inputValue.innerText = inputValue.innerText.substring(0, inputValue.innerText.length - 1);
+            if (inputValue.innerText.length == 0) {
+                inputValue.innerText = "0";
+            }
+        } else {
+            if (!isNaN(lastValue)) {
+                inputValue.innerText += e.target.innerText;
+            }
+        }
+    });
+});
+
+const number = document.querySelectorAll(".numbers").forEach(function (item) {
+    item.addEventListener("click", function (e) {
+        if (inputValue.innerText === "0") {
+            inputValue.innerText = "";
+        }
+        inputValue.innerText += e.target.innerText.trim();
+    });
+});
