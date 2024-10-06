fc063a5a0eff3d24694af3bfc4aa6b45d2e4b2e4
Matus <matuskocik@gmail.com>
2024-10-06 20:12:59 +0200

    add comment with homework...

--- lesson_08/bonus/script.ts
+++ lesson_08/bonus/script.ts
@@ -1,3 +1,11 @@
+/*
+Data z API i případnou chybu vypište do webové stránky.
+Stránku pěkně nastylujte, aby byla zobrazovaná data přehledná a čitelná.
+Vytvořte stránku s detailem uživatele, který jako parametr vezme ID uživatele a na základě toho ID vypíše data o uživateli.
+Data o jednom uživateli načtete ze stránky např: https://jsonplaceholder.typicode.com/users/2
+Ošetřete, že ID uživatele existuje. V případě neexistujícího ID vypište správnou chybovou hlášku včetně správného HTTP Error kódu
+*/
+
 import { UserPerson } from "./interfaces";
 
 const apiUrl: string = "https://jsonplaceholder.typicode.com/users";
